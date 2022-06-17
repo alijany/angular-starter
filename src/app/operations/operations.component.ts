f848ea76b9a818348fafaa2ab47fa62c27f6ac95
Mohammad H Alijany <mh.alijany@gmail.com>
2022-06-17 18:12:43 +0430

    generate oprations component

---
+++ src/app/operations/operations.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core'
+
+@Component({
+  selector: 'app-operations',
+  templateUrl: './operations.component.html',
+  styleUrls: ['./operations.component.scss']
+})
+export class OperationsComponent implements OnInit {
+  ngOnInit (): void {
+  }
+}
